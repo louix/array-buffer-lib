bca114aa04457a127a042431561b6acde15b67f9
louix <28579246+louix@users.noreply.github.com>
2022-01-22 13:56:56 +0000

    working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
 import { useEffect } from "react";
-import { mkArrayBuffer } from "./arraybuffer.lib";
 import * as AB from "./arraybuffer.lib";
-
-const fillBufferRandomly = (buffer: ArrayBuffer, arrayBufferObject: AB.ArrayBufferParams) => {
-  let lastOffset = 0;
-  Object.values(arrayBufferObject).forEach(({ kind, length }) => {
-    const arr = Array.from({ length }).map((_, i) => i + 1);
-    new kind(buffer).set(arr as any, lastOffset)
-    lastOffset += kind.BYTES_PER_ELEMENT * length
-  })
-}
 
 export const App = () => {
   useEffect(() => {
     const arrayBufferObject = {
       one: {
         kind: Uint8Array,
-        length: 2
+        length: 5
       }, two: {
         kind: Uint16Array,
         length: 2
       }, three: {
         kind: Uint32Array,
-        length: 2
+        length: 3
       }
     };
-    const buffer = AB.mkArrayBuffer(arrayBufferObject, 3);
-    fillBufferRandomly(buffer, arrayBufferObject);
+    const buffer = AB.mkArrayBuffer(arrayBufferObject, 1);
+    console.log(buffer.byteLength);
+    AB.debugFillBuffer(buffer, arrayBufferObject);
 
-    const getData = AB.mkGetData(buffer, arrayBufferObject);
-    const data = getData(0)
+    const accessData = AB.mkAccessData(buffer, arrayBufferObject);
+    const data = accessData(0)
     console.log(data.one, data.two, data.three);
 
   }, [])
